ff77de778e4064d25d74a8513b31b10f6cd2234f
Daria Chelombeeva <chelombeeva@gmail.com>
2020-10-09 14:44:04 +0300

    seconde version

--- hashtag_mining.py
+++ hashtag_mining.py
@@ -1,16 +1,19 @@
 import tweepy
-import settings
-
-
-hashtag = input('Input your hashtag starting with "#"', )
-
+import settings 
 auth = tweepy.OAuthHandler(settings.API_key, settings.API_secret_key)
 
-api = tweepy.API(auth)
+def text_comments_mining(hashtag):
+    
+    api = tweepy.API(auth)
+    
+    for tweet in tweepy.Cursor(api.search, q=hashtag, rpp=100).items(5):
+        print(tweet.text)
+        pass
+    return hashtag
 
-for tweet in tweepy.Cursor(api.search, q=hashtag, rpp=100).items():
-    print(tweet.text)
-    pass
+if __name__=="__main__":
+    hashtag = input('Input your hashtag starting with "#"' , )
+    text_comments_mining(hashtag)
 
 
 
