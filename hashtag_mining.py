98e32be2128d7012db75838b6ec63cec39216617
Daria Chelombeeva <chelombeeva@gmail.com>
2020-10-10 15:28:11 +0300

    changed function of data mining

--- hashtag_mining.py
+++ hashtag_mining.py
@@ -5,15 +5,13 @@ auth = tweepy.OAuthHandler(settings.API_key, settings.API_secret_key)
 def text_comments_mining(hashtag):
     
     api = tweepy.API(auth)
-    
+    all_tweets = []
     for tweet in tweepy.Cursor(api.search, q=hashtag, rpp=100).items(5):
-        print(tweet.text)
-        pass
-    return hashtag
+        all_tweets.append(tweet.text)
+    return all_tweets
 
 if __name__=="__main__":
     hashtag = input('Input your hashtag starting with "#"' , )
-    text_comments_mining(hashtag)
-
-
+    tweets = text_comments_mining(hashtag)
+    print(len(tweets))
 
