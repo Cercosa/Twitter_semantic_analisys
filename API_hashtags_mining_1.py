49fe728977e1b4d2671d0006a199160bd7bf5f2a
Daria Chelombeeva <chelombeeva@gmail.com>
2020-10-27 12:16:06 +0300

    modifide

--- API_hashtags_mining_1.py
+++ API_hashtags_mining_1.py
@@ -1,17 +1,17 @@
 import tweepy
-import settings 
+import settings
 auth = tweepy.OAuthHandler(settings.API_key, settings.API_secret_key)
 
+
 def text_comments_mining(hashtag):
-    
     api = tweepy.API(auth)
     all_tweets = []
     for tweet in tweepy.Cursor(api.search, q=hashtag, rpp=100).items(5):
         all_tweets.append(tweet.text)
     return all_tweets
 
-if __name__=="__main__":
-    hashtag = input('Input your hashtag starting with "#"' , )
+
+if __name__ == "__main__":
+    hashtag = input('Input your hashtag starting with "#"', )
     tweets = text_comments_mining(hashtag)
     print(len(tweets))
-
