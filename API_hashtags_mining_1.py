9470c7d3660cb8e90042c28d253692147a9e4ee7
Svetlana <svetlana.stepanova.314@gmail.com>
2020-10-28 19:26:36 +0800

    analysis moved to Mining

--- API_hashtags_mining_1.py
+++ API_hashtags_mining_1.py
@@ -1,17 +1,20 @@
 import tweepy
 import settings
+from sentiment_analysis import sentiment_analysis
+from model import db, Tweets_sentiment
 auth = tweepy.OAuthHandler(settings.API_key, settings.API_secret_key)
 
 
 def text_comments_mining(hashtag):
     api = tweepy.API(auth)
-    all_tweets = []
-    for tweet in tweepy.Cursor(api.search, q=hashtag, rpp=100).items(5):
-        all_tweets.append(tweet)
-    return all_tweets
+    for tweet in tweepy.Cursor(api.search, q=hashtag, rpp=100).items():
+        result = sentiment_analysis(tweet.text)
+        print(f"Tweet: {tweet.text}.\nResult of analysis: {result}.")
+        new_tweet = Tweets_sentiment(hashtag=hashtag, tweet_id=int(tweet.id), tweet_date=tweet.created_at, sentiment=float(result))
+        db.session.add(new_tweet)
+        db.session.commit()
 
 
 if __name__ == "__main__":
     hashtag = input('Input your hashtag starting with "#"', )
     tweets = text_comments_mining(hashtag)
-    print(len(tweets))
